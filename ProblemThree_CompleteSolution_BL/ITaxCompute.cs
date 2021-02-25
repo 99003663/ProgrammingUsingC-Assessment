6c12a01452f69899f807baee396b938ff870a43e
99003663 <78896013+99003663@users.noreply.github.com>
2021-02-25 17:30:37 +0530

    Update ITaxCompute.cs

--- ProblemThree_CompleteSolution_BL/ITaxCompute.cs
+++ ProblemThree_CompleteSolution_BL/ITaxCompute.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +9,14 @@ namespace ProblemThree_CompleteSolution_BL
     interface ITaxCompute
     {
         double TaxOnTotalBill();
+    }
+    class Tax:ITaxCompute
+    {
+        public double TotalBillValue { get; set; }
+        public double TaxOnTotalBill()
+        {
+            TotalBillValue = TotalBillValue + (0.07);
+            return TotalBillValue;
+        }
     }
 }
